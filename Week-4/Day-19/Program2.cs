abd2e72712eb5a7fe01ab783f3aba7f1004eb6ba
Vishnu Nair <vn75670@gmail.com>
2026-03-12 16:36:22 +0530

    Day 19 Probelm 2 Done

---
+++ Week-4/Day-19/Program2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Calculator
+{
+    // Method for Addition
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    // Method for Subtraction
+    public int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        int num1, num2;
+
+        Console.Write("Enter first number: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        // Creating object of Calculator class
+        Calculator calc = new Calculator();
+
+        // Calling methods
+        int addition = calc.Add(num1, num2);
+        int subtraction = calc.Subtract(num1, num2);
+
+        // Displaying output
+        Console.WriteLine("Addition = " + addition);
+        Console.WriteLine("Subtraction = " + subtraction);
+    }
+}
